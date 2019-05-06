de080ac48eac0f1c9b2ab304d5e2898e8af4228f
lichun <atsushinee@outlook.com>
2019-05-06 11:26:10 +0800

    增加 hystrix dashboard 服务熔断监控 http://ip:port/hystrix

--- dubbo-service-consumer/src/main/java/com/lichunorz/consumer/DubboServiceConsumerApplication.java
+++ dubbo-service-consumer/src/main/java/com/lichunorz/consumer/DubboServiceConsumerApplication.java
@@ -3,8 +3,10 @@ package com.lichunorz.consumer;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.hystrix.EnableHystrix;
+import org.springframework.cloud.netflix.hystrix.dashboard.EnableHystrixDashboard;
 
 @EnableHystrix
+@EnableHystrixDashboard
 @SpringBootApplication
 public class DubboServiceConsumerApplication {
 
